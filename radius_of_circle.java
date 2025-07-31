2e57d4f91aed5a8413f8d2dc119f42dd5ae4bbe1
Shankha2003 <pariashankhadeep@gmail.com>
2025-07-31 13:17:35 +0530

    Java Basics

---
+++ radius_of_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class radius_of_circle {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the radius of the Circle : ");
+        int radius = sc.nextInt();
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of the Circle is " + area );
+     }
+}
