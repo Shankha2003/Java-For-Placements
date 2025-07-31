8e2fa7371e76b2f9a5d6d6d669e082a52b499876
Shankha2003 <pariashankhadeep@gmail.com>
2025-07-31 13:05:38 +0530

    first commit

---
+++ Day 1/java_input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class java_input {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("write your name here : " );
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+    
+}
---
+++ Day 1/java_variables.java
@@ -0,0 +1,18 @@
+public class java_variables {
+    public static void main(String[] args) {
+        String name = "Shankha";
+        int age = 23;
+        double CGPA = 7.45;
+
+        System.out.println("hello " + name +" Age :" + age + "CGPA :" + CGPA );
+
+
+        int a = 8;
+        int b = 12;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+    
+}
