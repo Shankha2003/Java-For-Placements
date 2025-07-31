17f83d28bcb06b1484ad65b0ea16cf33b93d9dec
Shankha2003 <pariashankhadeep@gmail.com>
2025-07-31 13:30:00 +0530

    #

--- Day 1/radiusOfCircle.java
+++ Day 1/radiusOfCircle.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class radius_of_circle {
+public class radiusOfCircle {
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Write the radius of the Circle : ");
