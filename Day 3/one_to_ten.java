788a1f1dadb55aee53fc74de93396884bcab2743
Shankhadeep Paria <pariashankhadeep@gmail.com>
2025-08-03 12:18:11 +0530

    #

---
+++ Day 3/one_to_ten.java
@@ -0,0 +1,10 @@
+
+public class one_to_ten {
+    public static void main(String[] args) {
+        //counter=counter + 1 => counter++
+        for(int counter = 1 ; counter < 11 ; counter++){
+            System.out.println(counter);
+        }
+    }
+    
+}
